c7c90ea9d5f13d8f8ae4135e71b26cfcf49a3921
vijay kumar <rameshvijay2004@gmail.com>
2024-07-23 11:44:56 +0530

    comm

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Student-details/', // This is the base URL of your GitHub Pages site
+
+})
